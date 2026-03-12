74ef13ed1845335814f5ba0f51ce32ec9cc22b09
ant1Da <antoine.dast@edouardgand.fr>
2026-03-12 15:48:09 +0100

    Exo 1 classe Movie

---
+++ Movie.cs
@@ -0,0 +1,21 @@
+﻿namespace TP_UML_C__GIT;
+
+class Movie
+{
+    private string title;
+    private int duration;
+    public string Title
+    {
+        get {return title;}
+        set {title = value;}
+    }
+    public int Duration
+    {
+        get {return duration;}
+        set {duration = value;}
+    }
+    public void Display()
+    {
+        Console.WriteLine($"Titre : {this.Title} | Durée : {this.Duration}");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace TP_UML_C__GIT;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Movie film1 = new Movie();
+        film1.Title = "La chatte à ta mère";
+        film1.Duration = 126;
+        film1.Display();
+    }
+}
